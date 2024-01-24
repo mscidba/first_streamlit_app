bf8dab6f5ff1ecdf31da8ea6adf4942686985f0c
Jeff Pell <mscidba@users.noreply.github.com>
2024-01-24 10:33:45 -0500

    Update streamlit_app.py
    02 - Add additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 #Created the main Python file
 import streamlit
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, spinach and Rocket smoothie')
+
+streamlit.text('Hard-boiled free-range egg')
